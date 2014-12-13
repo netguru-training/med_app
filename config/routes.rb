3d6bf28848bbb1387a68c1b2debb1ef0910006ad
sneaky22 <jsamol22@wp.pl>
2014-12-13 11:30:07 +0100

    Simple client summary view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
-  resources :users
+  resources :users do
+    get :summary, as: :member
+  end
   get 'dashboard', to: 'dashboard#index'
 end
