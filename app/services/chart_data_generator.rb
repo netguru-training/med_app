9405e935fc8314b95b57d49deab52e2e8c3d543c
Zofia Zmojdzin <zosiazmojdzin@gmail.com>
2014-12-13 16:56:20 +0100

    color changes

--- app/services/chart_data_generator.rb
+++ app/services/chart_data_generator.rb
@@ -11,26 +11,30 @@ class ChartDataGenerator
     blood_pressure_keys = keys.shift(2)
     keys.each do |type|
       result[type] = {
-        labels: entries.public_send(type).pluck(:date) ,
+        labels: entries.public_send(type).pluck(:date).map {|d| d.to_date} ,
         datasets: [
+          fillColor: "rgba(151,187,205,0.5)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
           data: entries.public_send(type).pluck(:value)
         ]
       }
     end
     result[:blood_pressure] = {
-      labels: (entries.blood_pressure_in.pluck(:date) + entries.blood_pressure_out.pluck(:date)).uniq,
+      labels: (entries.blood_pressure_in.pluck(:date) + entries.blood_pressure_out.pluck(:date)).uniq.map {|d| d.to_date} ,
       datasets: [
         {
-          fillColor: "rgba(220,220,220,0.5)",
+          fillColor: "rgba(186,57,57,0.5)",
           strokeColor: "rgba(220,220,220,1)",
           pointColor: "rgba(220,220,220,1)",
           pointStrokeColor: "#fff",
           data: entries.blood_pressure_in.pluck(:value)
         },
         {
-          fillColor: "rgba(151,187,205,0.5)",
-          strokeColor: "rgba(151,187,205,1)",
-          pointColor: "rgba(151,187,205,1)",
+          fillColor: "rgba(61,100,168,0.5)",
+          strokeColor: "rgba(61,100,168,1)",
+          pointColor: "rgba(61,100,168,1)",
           pointStrokeColor: "#fff",
           data: entries.blood_pressure_out.pluck(:value)
         }
