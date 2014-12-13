38510ac623700327b9675cfc7742276530dee224
Olga <olgadraber@gmail.com>
2014-12-13 15:25:13 +0100

    set gon and chart_data variable for patient dashboard charts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,17 @@ class DashboardController < ApplicationController
 
   before_action :authenticate_user!
 
+  expose(:entries) { current_user.entries }
+
+  def index
+    gon.chart_data = { }
+
+    entries.examination_types.keys.each do |type|
+      gon.chart_data[type] = {
+        labels: entries.public_send(type).pluck(:date) ,
+        values: entries.public_send(type).pluck(:value)
+      }
+    end
+  end
+
 end
