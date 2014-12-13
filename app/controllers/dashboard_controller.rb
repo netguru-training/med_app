d73a7e307e9bb0e9f10a73886a9926b02f1016a5
Zofia Zmojdzin <zosiazmojdzin@gmail.com>
2014-12-13 16:12:55 +0100

    create service chart data generator

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,17 +2,10 @@ class DashboardController < ApplicationController
 
   before_action :authenticate_user!
 
-  expose(:entries) { current_user.entries }
+  expose_decorated(:entries) { current_user.entries }
 
   def index
-    gon.chart_data = { }
-
-    entries.examination_types.keys.each do |type|
-      gon.chart_data[type] = {
-        labels: entries.public_send(type).pluck(:date) ,
-        values: entries.public_send(type).pluck(:value)
-      }
-    end
+    gon.chart_data = ChartDataGenerator.new(entries).generate
   end
 
 end
